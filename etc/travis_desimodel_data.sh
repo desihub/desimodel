272a84c4a9d8f0025a9b1f5997a6fb45b56bdfaa
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-01-30 01:02:13 -0500

    make sure cd command doesn't affect main script

--- etc/travis_desimodel_data.sh
+++ etc/travis_desimodel_data.sh
@@ -2,6 +2,6 @@
 [[ ${DEBUG} == True ]] && set -x
 export DESIMODEL=${HOME}/desimodel/${DESIMODEL_VERSION}
 mkdir -p ${DESIMODEL}
-cd ${DESIMODEL}
-svn export https://desi.lbl.gov/svn/code/desimodel/${DESIMODEL_VERSION}/data
+# Do this in a subshell, so the directory change doesn't affect the main script.
+(cd ${DESIMODEL} && svn export https://desi.lbl.gov/svn/code/desimodel/${DESIMODEL_VERSION}/data)
 [[ ${DEBUG} == True ]] && set +x
