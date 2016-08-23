f6b276f9c7c57e2784794dc516876cf51a2fd077
Benjamin Alan Weaver <baweaver@lbl.gov>
2016-08-23 14:51:22 -0700

    test rebin function

--- py/desimodel/test/test_trim.py
+++ py/desimodel/test/test_trim.py
@@ -5,7 +5,7 @@
 from os.path import abspath, dirname
 import unittest
 from .. import __version__ as desimodel_version
-from ..trim import inout
+from ..trim import inout, rebin_image
 
 
 class TestTrim(unittest.TestCase):
@@ -17,6 +17,14 @@ class TestTrim(unittest.TestCase):
         """
         self.assertEqual(inout('/in', '/out', 'file.txt'),
                          ('/in/file.txt', '/out/file.txt'))
+
+    def test_rebin_image(self):
+        """Test image rebinning.
+        """
+        image = np.ones((10, 10), dtype='i2')
+        i1 = rebin_image(image, 5)
+        i2 = np.array([[25, 25], [25, 25]], dtype='i2')
+        self.assertTrue((i1 == i2).all())
 
 
 if __name__ == '__main__':
