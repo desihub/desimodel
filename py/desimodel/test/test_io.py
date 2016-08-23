9a859b6f18f7417c27396b731bcc452648561cbe
Benjamin Alan Weaver <baweaver@lbl.gov>
2016-08-23 10:40:48 -0700

    Fix test

--- py/desimodel/test/test_io.py
+++ py/desimodel/test/test_io.py
@@ -96,18 +96,19 @@ class TestIO(unittest.TestCase):
         """Test grabbing tile information by tileID.
         """
         io_tile_cache = io._tiles
-        tiles = np.zeros((4,), dtype=[('TILEID', 'i2'),
-                                      ('RA', 'f8'),
-                                      ('DEC', 'f8'),
-                                      ('IN_DESI', 'i2')])
-        tiles['TILEID'] = np.arange(4) + 1
-        tiles['RA'] = [0.0, 1.0, 2.0, 3.0]
-        tiles['DEC'] = [-2.0, -1.0, 1.0, 2.0]
-        tiles['IN_DESI'] = [0, 1, 1, 0]
+        tiles = np.recarray(np.zeros((4,), dtype=[('TILEID', 'i2'),
+                                                  ('RA', 'f8'),
+                                                  ('DEC', 'f8'),
+                                                  ('IN_DESI', 'i2')]))
+        t = tiles.view(np.recarray)
+        t.TILEID = np.arange(4) + 1
+        t.RA = [0.0, 1.0, 2.0, 3.0]
+        t.DEC = [-2.0, -1.0, 1.0, 2.0]
+        t.IN_DESI = [0, 1, 1, 0]
         io._tiles = tiles
-        ra, dec = io.get_tile_radec(0)
+        ra, dec = io.get_tile_radec(1)
         self.assertEqual((ra, dec), (0.0, 0.0))
-        ra, dec, = io.get_tile_radec(1)
+        ra, dec, = io.get_tile_radec(2)
         self.assertEqual((ra, dec), (1.0, -1.0))
         io._tiles = io_tile_cache
 
