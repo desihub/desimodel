a6e2588ea938b368be731ea7896e7e98b5b18b61
Benjamin Alan Weaver <baweaver@lbl.gov>
2016-08-23 11:14:15 -0700

    Add tests for install.py

---
+++ py/desimodel/test/test_install.py
@@ -0,0 +1,42 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""Test desimodel.install.
+"""
+from os.path import abspath, dirname
+import unittest
+from .. import __version__ as desimodel_version
+from ..install import default_install_dir, svn_export, install
+
+
+class TestInstall(unittest.TestCase):
+    """Test desimodel.install.
+    """
+
+    def test_default_install_dir(self):
+        """Test setting default install directory.
+        """
+        d1 = default_install_dir()
+        d2 = dirname(dirname(dirname(dirname(dirname(dirname(__file__))))))
+        self.assertEqual(abspath(d1), abspath(d2))
+
+    def test_svn_export(self):
+        """Test svn export command.
+        """
+        base_url = "https://desi.lbl.gov/svn/code/desimodel/{0}/data"
+        cmd = svn_export()
+        self.assertEqual(cmd[2], base_url.format(desimodel_version))
+        cmd = svn_export('trunk')
+        self.assertEqual(cmd[2], base_url.format('trunk'))
+        cmd = svn_export('branches/v3')
+        self.assertEqual(cmd[2], base_url.format('branches/v3'))
+        cmd = svn_export('1.2.3')
+        self.assertEqual(cmd[2], base_url.format('tags/1.2.3'))
+
+    def test_install(self):
+        """Test the install function.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
