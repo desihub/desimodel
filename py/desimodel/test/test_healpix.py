340d7a794d69531d58794ce5c10b8d05774f75a7
John Moustakas <john.moustakas@gmail.com>
2017-05-31 06:26:04 -0400

    add unit test

---
+++ py/desimodel/test/test_healpix.py
@@ -0,0 +1,30 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""Test desimodel.healpix
+"""
+from __future__ import print_function, division
+
+import unittest
+import numpy as np
+from desimodel.healpix import tiles2pix
+
+class TestHealpix(unittest.TestCase):
+    """Test desimodel.healpix.
+    """
+    def test_tiles2pix(self):
+        """Test that the correct healpix tiles are returned for a couple values of nside.
+        """
+        import desimodel.io
+        tiles = desimodel.io.load_tiles()
+
+        pix = tiles2pix(nside=8, tiles=tiles[:3], radius=1.6)
+        self.assertTrue( np.all(pix==[196,197,208,302,303]) )
+
+        pix = tiles2pix(nside=16, tiles=tiles[:3], radius=1.6)
+        self.assertTrue( np.all(pix==[785,788,789,791,832,1209,1211,1214,1215]) )
+
+def test_suite():
+    """Allows testing of only this module with the command::
+        python setup.py test -m <modulename>
+    """
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
