289bd1a6fcdb3f2e2cdd6ec30ee0161161d7bb16
Benjamin Alan Weaver <baweaver@lbl.gov>
2017-05-31 17:19:30 -0700

    fix documentation problem

--- doc/conf.py
+++ doc/conf.py
@@ -131,7 +131,8 @@ napoleon_include_private_with_doc = True
 # This value contains a list of modules to be mocked up. This is useful when
 # some external dependencies are not met at build time and break the
 # building process.
-autodoc_mock_imports = ['numpy', 'astropy.io', 'astropy.io.fits', 'astropy.units',
+autodoc_mock_imports = ['numpy', 'astropy.io', 'astropy.io.fits', 
+                        'astropy.table', 'astropy.units',
                         'specter', 'specter.throughput', 'specter.psf',
                         'scipy.signal', 'scipy.special']
 
