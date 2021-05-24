880547930673a6ca08508afa537b1c43fd9621b7
Theodore Kisner <tskisner.public@gmail.com>
2021-05-23 22:04:32 -0700

    Merge sync scripts into one

--- etc/desimodel_sync_kpno_cron.sh
+++ etc/desimodel_sync_kpno_cron.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Exit on error
-set -e
+# Ensure that we are running this as the datasystems user
+if [ $(whoami) != "datasystems" ]; then
+    echo "You should only run this script at KPNO as the datasystems user"
+    exit 1
+fi
 
 # Path to the sync directory, containing the desimodel data trunk
 # checkout and logs.
@@ -18,11 +21,11 @@ echo "Running at ${logdate}" > "${logfile}"
 # Get the latest desiconda install
 desiconda=$(ls -d /software/datasystems/desiconda/20* | sort | tail -n 1)
 
-# Get the latest KPNO version of desimodules
-desimodules=$(ls ${desiconda}/modulefiles/desimodules/*-kpno | sort | tail -n 1 | xargs basename)
+# Get the latest stable version of desimodules
+desimodules=$(ls -d ${desiconda}/modulefiles/desimodules/2* | sort -V | tail -n 1 | xargs basename)
 
 echo "Using latest desiconda:  ${desiconda}" >> "${logfile}"
-echo "Using latest KPNO version of desimodules:  ${desimodules}" >> "${logfile}"
+echo "Using latest stable version of desimodules:  ${desimodules}" >> "${logfile}"
 
 # Set up environment
 
@@ -38,10 +41,27 @@ module load desimodules/${desimodules}
 echo "Using desimodel data svn trunk at ${svntrunk}" >> "${logfile}"
 export DESIMODEL="${svntrunk}"
 
-# Find our sync script and run it.
-runsync=$(which desimodel_sync_kpno.sh)
-echo "Calling sync script:  ${runsync}" >> "${logfile}"
+# Find the sync script
+fpsync=$(which desi_sync_focalplane)
+echo "Using focalplane sync script:  ${fpsync}" >> "${logfile}"
 
-eval ${runsync} >> "${logfile}"
+# Find the newest calibration file
+caldir="/data/focalplane/calibration/"
+calfile=$(ls ${caldir} | egrep '[0-9]{8}T[0-9]{6}.*' | sort | tail -n 1)
+calpath="${caldir}/${calfile}"
+echo "Found newest calibration file:  ${calpath}" >> "${logfile}"
+
+# Run it.
+failed="no"
+eval ${fpsync} --calib_file ${calpath} --commit >> "${logfile}"
+if [ $? -ne 0 ]; then
+    failed="yes"
+    echo "Focalplane sync failed" >> "${logfile}"
+fi
+
+# Send notifications
+
+
+
 
 echo "Sync script finished at $(date -u --iso-8601=seconds)" >> "${logfile}"
